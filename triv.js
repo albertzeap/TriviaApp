c4d1ededd888e59d46cb0b009c1dfde84a8f209d
Albert <albertzeap@gmail.com>
2022-09-17 12:05:26 -0700

    rendered all available questions and answers

--- triv.js
+++ triv.js
@@ -19,11 +19,46 @@ let fetchQuestion = async () => {
 
 }
 
-let questions_array = document.getElementById("questions-container");
+let question_container = document.getElementById("questions-container");
+let question_prompt = document.getElementById("question-prompt");
+
+
+// Traverses through the array of answers
+let createOptions = (incorrect, correct, ul) => {
+    let combinedAnswers = new Array();
+
+    // Push all answers into a new array
+    incorrect.forEach(incorrectAns => {
+        combinedAnswers.push(incorrectAns);
+    });
+    combinedAnswers.push(correct);
+    combinedAnswers.sort();
+
+    // Display unordered list of buttons for answers
+    combinedAnswers.forEach(option =>{
+        let optionLi = document.createElement("button");
+        optionLi.innerHTML = option;
+        ul.append(optionLi);
+    });
+}
 
 let renderQuestions = () => {
     fetchQuestion().then((questionObject) => {
-        questions_array.innerHTML = questionObject[0].question;
+
+        // Traverses through each question array in the object
+        questionObject.forEach(question => {
+            let question_ = document.createElement("h4");
+            let options_list = document.createElement("ul");
+            question_.innerHTML = question.question;
+
+            createOptions(question.incorrectAnswers,question.correctAnswer, options_list);
+
+
+            // Append all the questions and answers into the questions container
+            question_container.append(question_);
+            question_container.append(options_list);
+            // console.log(question.question);
+        });
         console.log(questionObject);
     });
 }
